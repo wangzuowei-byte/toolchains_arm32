36b44b14fedc36509dc685b3c98da8b1747bc188
zuowei.wang <zuowei.wang@jicaai.com>
2025-04-30 16:51:29 +0800

    add mqtt

--- scripts/gcc_arm_linaro_linux_4.9.4_2017.01.sh
+++ scripts/gcc_arm_linaro_linux_4.9.4_2017.01.sh
@@ -10,13 +10,16 @@ function install_armcc()
     local workspace_path=$1
     local install_path=$2
 
+    local toolchains_path=${install_path}/${TOOLCHAINS_BASH_PATH}/${TOOLCHAINS_INSATLL_DIR}
+
+
     if [ -d ${install_path}/${TOOLCHAINS_BASH_PATH} ]; then
         rm -rf ${install_path}/${TOOLCHAINS_BASH_PATH}
         log_check_return "rm -rf ${install_path}/${TOOLCHAINS_BASH_PATH}"
     fi
 
-    mkdir -p ${install_path}/${TOOLCHAINS_BASH_PATH}/${TOOLCHAINS_INSATLL_DIR}
-    log_check_return "mkdir ${install_path}/${TOOLCHAINS_BASH_PATH}/${TOOLCHAINS_INSATLL_DIR}"
+    mkdir -p ${toolchains_path}
+    log_check_return "mkdir ${toolchains_path}"
 
     cp -a ${workspace_path}/src/${TOOLCHAINS_BASH_PATH} ${install_path}/
     log_check_return "cp ${workspace_path}/src/${TOOLCHAINS_BASH_PATH} -> install_path/"
@@ -25,10 +28,11 @@ function install_armcc()
     log_check_return "cp ${workspace_path}/src/scripts -> install_path/"
 
 
-    Decompression_tar_xz    ${install_path}/${TOOLCHAINS_BASH_PATH}/${TOOLCHAINS_INSATLL_DIR} \
+    Decompression_tar_xz    ${toolchains_path} \
                             ${workspace_path}/pack_tar/${TOOLCHAINS_TAR}
 
-
+    cp -a ${workspace_path}/sdk_path/* ${toolchains_path}/arm-linux-gnueabihf/libc/
+    log_check_return "cp sdk ->  ${toolchains_path}/"
 
     return 0
 }
--- scripts/gcc_arm_none_linux_9.2_2019.12.sh
+++ scripts/gcc_arm_none_linux_9.2_2019.12.sh
@@ -50,13 +50,15 @@ function install_armcc()
     local workspace_path=$1
     local install_path=$2
 
+    local toolchains_path=${install_path}/${TOOLCHAINS_BASH_PATH}/${TOOLCHAINS_INSATLL_DIR}
+
     if [ -d ${install_path}/${TOOLCHAINS_BASH_PATH} ]; then
         rm -rf ${install_path}/${TOOLCHAINS_BASH_PATH}
         log_check_return "rm -rf ${install_path}/${TOOLCHAINS_BASH_PATH}"
     fi
 
-    mkdir -p ${install_path}/${TOOLCHAINS_BASH_PATH}/${TOOLCHAINS_INSATLL_DIR}
-    log_check_return "mkdir ${install_path}/${TOOLCHAINS_BASH_PATH}/${TOOLCHAINS_INSATLL_DIR}"
+    mkdir -p ${toolchains_path}
+    log_check_return "mkdir ${toolchains_path}"
 
     cp -a ${workspace_path}/src/${TOOLCHAINS_BASH_PATH} ${install_path}/
     log_check_return "cp ${workspace_path}/src/${TOOLCHAINS_BASH_PATH} -> install_path/"
@@ -65,15 +67,19 @@ function install_armcc()
     cp -a ${workspace_path}/src/scripts ${install_path}/
     log_check_return "cp ${workspace_path}/src/scripts -> install_path/"
     
-    Decompression_tar_xz    ${install_path}/${TOOLCHAINS_BASH_PATH}/${TOOLCHAINS_INSATLL_DIR} \
+    Decompression_tar_xz    ${toolchains_path} \
                             ${workspace_path}/pack_tar/${TOOLCHAINS_TAR}
 
+    cp -a ${workspace_path}/sdk_path/* ${toolchains_path}/arm-none-linux-gnueabihf/libc/
+    log_check_return "cp sdk ->  ${toolchains_path}/"
+    
     cp ${workspace_path}/pack_tar/${TOOLCHAINS_STM_SRC_NAME}.zip ${install_path}/${TOOLCHAINS_BASH_PATH}/
     log_check_return "cp ${workspace_path}/pack_tar/${TOOLCHAINS_STM_SRC_NAME}.zip -> install_path/"
 
     install_stm_tools ${install_path}/${TOOLCHAINS_BASH_PATH} ${TOOLCHAINS_STM_SRC_NAME}
 
-    link_stm_tools ${TOOLCHAINS_BIN_NAME} ${TOOLCHAINS_STM_SRC_NAME} ${install_path}/${TOOLCHAINS_BASH_PATH}/${TOOLCHAINS_INSATLL_DIR}
+    link_stm_tools ${TOOLCHAINS_BIN_NAME} ${TOOLCHAINS_STM_SRC_NAME} ${toolchains_path}
+
 
 
     return 0
